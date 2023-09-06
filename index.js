da17884b5db826543bf1ee533982dce1a84457c6
dopalescent <skudlgre000@gmail.com>
2023-09-06 14:02:08 -0400

    started seeds.sql, got one query to test successful, confidence bolstered

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const inquirer = require('inquirer');
-const mysql = requre('mysql2');
-const cTab = require('console.table');
+// const mysql = requre('mysql2');
+// const cTable = require('console.table');
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Existence700!",
-  database: "tracker_db"
-},
-  console.log('Connected to database.')
-);
+const { queries } = require('./lib/query');
+
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "Existence700!",
+//   database: "tracker_db"
+// });
